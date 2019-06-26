699f9ef850bacea0bdfd436a8a7ff8f71a8b6ab9
Dustin Quam <quamsta@gmail.com>
2019-06-26 11:54:30 -0500

    fixed use statement

--- mysite/_config.php
+++ mysite/_config.php
@@ -3,7 +3,7 @@
 use SilverStripe\Security\PasswordValidator;
 use SilverStripe\Security\Member;
 use SilverStripe\Control\Director;
-
+use SilverStripe\Forms\HTMLEditor\HTMLEditorConfig;
 // remove PasswordValidator for SilverStripe 5.0
 $validator = new PasswordValidator();
 
